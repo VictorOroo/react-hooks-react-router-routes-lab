a91d317dead6246163f8d4d21d77eb142ea4499e
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-27 00:12:17 +0300

    done lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import ActorsPage from "./Actors";
+import DirectorsPage from "./Directors";
+import MoviesPage from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/actors" component={ActorsPage} />
+        <Route path="/directors" component={DirectorsPage} />
+        <Route path="/movies" component={MoviesPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
